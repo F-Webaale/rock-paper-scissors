14ff78423b95c471b64cb1bfc4cd86fae0e98ab7
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-02-25 00:00:10 +0300

    Create a function, that allows users to provide input. To display a certain choice in the game

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -2,7 +2,7 @@ console.log("Hello World!");
 
 // Function should return "paper" or "rock" or "scissors"
 // Fuction for computers choice is random
-// let choice range between 0 and 3
+// let choice range between 0, 1 and 2
 // If choice is 0=rock, 1=paper, 2=scissors
 
 function getComputerChoice(max) {
@@ -21,3 +21,28 @@ function getComputerChoice(max) {
 }
 
 getComputerChoice();
+
+// Prompt human to input choice; Display range of choices for selection
+// choice will be in string format , convert to number
+// If number from range is selected, output option is displayed.
+// If number is out of range, display "selection out of range"
+
+function getHumanChoice(yourInput) {
+    
+    yourInput = prompt("*SELECT*   0: rock 1: paper 2: scissors ");
+
+    if (yourInput === 0) {
+        console.log("rock");
+    }
+    else if (yourInput === 1) {
+        console.log("paper");
+    }
+    else if (yourInput === 2) {
+        console.log("scissors");
+    }
+    else {
+        console.log("Your selection is out of range!");
+    }
+}
+
+getHumanChoice();

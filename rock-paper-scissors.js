a71eac3036d99ff51b827b86245cb539291f28ff
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-02-25 16:06:27 +0300

    Return value rather than log it in the console

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -5,22 +5,21 @@ console.log("Hello World!");
 // let choice range between 0, 1 and 2
 // If choice is 0=rock, 1=paper, 2=scissors
 
-function getComputerChoice(max) {
+function getComputerChoice(choice) {
 
-    max = Math.floor(Math.random() * 3);
+    choice = Math.floor(Math.random() * 3);
 
-    if (max === 0) {
-        console.log("rock");
+    if (choice === 0) {
+        return "rock";
     }
-    else if (max === 1) {
-        console.log("paper");
+    else if (choice === 1) {
+        return "paper";
     }
-    else if (max === 2) {
-        console.log("scissors");
+    else {
+        return "scissors";
     }
 }
 
-getComputerChoice();
 
 // Prompt human to input choice; Display range of choices for selection
 // choice will be in string format , convert to number
@@ -29,20 +28,70 @@ getComputerChoice();
 
 function getHumanChoice(yourInput) {
     
-    yourInput = prompt("*SELECT*   0: rock 1: paper 2: scissors ");
+    yourInput = Number(prompt("*SELECT*   0: rock 1: paper 2: scissors "));
 
     if (yourInput === 0) {
-        console.log("rock");
+        return "rock";
     }
     else if (yourInput === 1) {
-        console.log("paper");
+        return "paper";
     }
     else if (yourInput === 2) {
-        console.log("scissors");
+        return "scissors";
     }
     else {
-        console.log("Your selection is out of range!");
+        return "Your selection is out of range!";
     }
 }
 
-getHumanChoice();
+
+// Declare score player variables.
+let humanScore = 0;
+let computerScore = 0;
+
+// Create a function for one round for the game.
+// Two parameters to pick choices((Human & Computer) as argument.
+// Compare the results : Give output
+// If choice is 0=rock, 1=paper, 2=scissors :Each options has 3 choices, win, draw and loose;
+// Increment the side that has won : Print value and output "You win! Scissors beats paper."
+
+/*function playRound(humanChoice, computerChoice) {
+    
+    if ( humanChoice === 0 && computerChoice === 1) {
+        console.log("Relax! Paper beats rock.");
+    }
+
+    else if (humanChoice === 0 && computerChoice === 2) {
+        console.log("Well done! Rock beats scissors.");
+    }
+
+    else {
+        console.log("Relax! Rock cant beat rock");
+    }*/
+
+    /*if ( 1 === 0) {
+        console.log("Well done! Paper beats rock.");
+    }
+
+    else if (1 === 2) {
+        console.log("Scissors beats paper.");
+    }
+
+    else {
+        console.log("Relax! paper cant beat paper.");
+    }
+
+    if ( 2 === 0) {
+        console.log("Sorry! rock beats scissors.");
+    }
+
+    else if (2 === 1) {
+        console.log("Scissors beats paper.");
+    }
+
+    else {
+        console.log("Relax! scissors cant beat scissors.");
+    }
+}
+playRound(getHumanChoice, getComputerChoice);*/
+

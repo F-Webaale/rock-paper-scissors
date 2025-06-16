570e6ce6fd4a5c86f70389262563b2ed8d015cde
Frank Webaale <166954770+F-Webaale@users.noreply.github.com>
2025-06-16 17:39:57 +0300

    Remove comments

--- rock-paper-scissors.js
+++ rock-paper-scissors.js
@@ -1,54 +1,5 @@
 console.log("Hello World!");
-/*
 
-// Declare score player variables.
-let humanScore = 0;
-let computerScore = 0;
-
-// Create a function for one round for the game.
-// Two parameters to pick choices((Human & Computer) as argument.
-// Compare the results : Give output
-// If choice is rock, paper, scissors :Each options has 3 choices, win, draw and loose;
-// Increment the side that has won : Print value and output "You win! Scissors beats paper."
-
-
-function playRound(humanChoice, computerChoice) {
-
-
-
-
-//let me try mergr into one button
-    humanChoice = getHumanChoice(Event);
-    computerChoice = getComputerChoice(Event);
-
-    // choices picked
-    console.log(`Computer choice: ${computerChoice}`);
-    console.log(`Your choice: ${humanChoice}`);
-
-    if (humanChoice === computerChoice) {
-        console.log("Its a draw");
-        return "Draw";
-    }
-
-    if ((humanChoice === "rock" && computerChoice === "scissors") || 
-       (humanChoice === "paper" && computerChoice === "rock") ||
-       (humanChoice === "scissors" && computerChoice === "paper")
-    ) {
-        console.log(`You win! ${humanChoice} beats ${computerChoice}.`);
-        console.log(`Your score: ${++humanScore}`);
-        return "Human";
-    }
-    else {
-        console.log(`You lose! ${computerChoice} beats ${humanChoice}.`)
-        console.log(`Computer score: ${++computerScore}`);
-        return "Computer";
-    }
-
-}
-
-console.log("ROUND 1")
-playRound();
-*/
 const buttons = document.querySelector("#btns");
 const printCon = document.querySelector(".printContent");
 
